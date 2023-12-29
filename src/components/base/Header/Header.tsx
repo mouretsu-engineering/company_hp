204db8ca456a7f9a72bea33c5ccbf51c1dc5dad5
yujifunfunfun <tyuji051605160516@gmail.com>
2023-12-30 05:50:31 +0900

    PCの実装完了

--- src/components/base/Header/Header.tsx
+++ src/components/base/Header/Header.tsx
@@ -1,28 +1,40 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack } from "@chakra-ui/react";
 import Image from 'next/image'
-import { HeaderLinkButton } from "../../ui/Button/HeaderLinkButton";
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
+import { EmailIcon } from '@chakra-ui/icons'
 
 export default function Header() {
   return (
-    <Box as="header" position="relative">
-      <Image 
-        src="/images/light-gray-concrete-wall.jpg"
-        alt="Header Background" 
-        width={1950} 
-        height={800} 
-      />
-
-      <Flex
-        position="absolute"
-        top={10}
-        right={100}
-        direction="row"
-        p={4}
-      >
-        <HeaderLinkButton label="事業紹介" href="#about-us" />
-        <HeaderLinkButton label="メンバー紹介" href="#member" />
-        <HeaderLinkButton label="お問い合わせ" href="#contact" />
-      </Flex>
+    <Box as="header" position="sticky" top='0'>
+      <Box h='10vw' p='2.7vw'>
+        <Flex alignItems='center' h='100%' justify="space-between">
+          <Box w="22.5vw" h='2vw' position='relative'>
+            <Image
+              src="/images/company-logo.png"
+              alt="株式会社猛烈エンジニアリング"
+              layout="fill"
+              objectFit="cover"
+            />
+          </Box>
+          <HStack gap='2.8vw'>
+            <HStack gap='2.2vw' fontSize='1.1vw' fontWeight='bold'>
+              <Link as={NextLink} href='/home' color='black' >
+                会社案内
+              </Link>
+              <Link as={NextLink} href='/home' color='black' >
+                事業内容
+              </Link>
+              <Link as={NextLink} href='/home' color='black' >
+                メンバー
+              </Link>
+            </HStack>
+            <Box>
+              <Button bgColor='#E61515' colorScheme="red" borderRadius='50%' h='4.5vw' w='4.5vw' fontSize='2.2vw'><EmailIcon/></Button>
+            </Box>
+          </HStack>
+        </Flex>
+      </Box>
     </Box>
   );
 }
