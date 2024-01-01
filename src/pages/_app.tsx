c4cac48d73601faf1106cddcdf24b35cb60eaf2e
yujifunfunfun <tyuji051605160516@gmail.com>
2024-01-01 11:42:06 +0900

    feat: ディレクトリとファイル整備

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Providers } from './providers'
+import Header from '@/components/base/Header/Header'
+import Footer from '@/components/base/Footer/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </Providers>
+  )
 }
