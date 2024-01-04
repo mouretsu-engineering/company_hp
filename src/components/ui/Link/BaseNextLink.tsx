dbf203c39beb71781d221fc004ee21de109ef829
yujifunfunfun <tyuji051605160516@gmail.com>
2024-01-04 13:43:14 +0900

    feat: 微修正

--- src/components/ui/Link/BaseNextLink.tsx
+++ src/components/ui/Link/BaseNextLink.tsx
@@ -1,8 +1,9 @@
+import NextLink from 'next/link'
 import { Link, LinkProps } from '@chakra-ui/react'
 
 export const BaseNextLink: React.FC<LinkProps> = (props) => {
   return (
-    <Link {...props} >
+    <Link as={NextLink}  {...props} >
       {props.children}
     </Link>
   );
