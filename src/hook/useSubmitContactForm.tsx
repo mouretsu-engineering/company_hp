009934967c148e4abf497efc94d395044c47be81
yujifunfunfun <tyuji051605160516@gmail.com>
2024-01-12 22:08:10 +0900

    お問い合わせ成功メッセージ修正

--- src/hook/useSubmitContactForm.tsx
+++ src/hook/useSubmitContactForm.tsx
@@ -18,7 +18,7 @@ export const useSubmitContactForm = () => {
         showToast("送信に失敗しました", "お手数ですが、時間をおいて再度お試しください。", "error");
       } else {
         reset();
-        showToast("お問い合わせありがとうございます", "2営業日以内にメールにてご連絡をさせていただきます。", "success");
+        showToast("お問い合わせありがとうございます", "2営業日以内にメールにてご連絡させていただきます。", "success");
       }
     } catch (error) {
       console.log('error');
