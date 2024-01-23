0a303fff5713a29578575e3f47131382c735dd68
yujifunfunfun <tyuji051605160516@gmail.com>
2024-01-23 23:59:21 +0900

    gitエラー対応

--- src/components/pages/company/Message.tsx
+++
@@ -1,8 +0,0 @@
-import { Philosophy } from "./Philosophy";
-
-export const Message = () => {
-
-  return (
-    <Philosophy label='MESSAGE' title='メッセージが入ります' text='こちらのセクションにはコンセプト、メッセージが入ると良いかと思います。こちらのセクションにはコンセプト、メッセージが入ると良いかと思います。こちらのセクションにはコンセプト、メッセージが入ると良いかと思います。' />
-  );
-}
