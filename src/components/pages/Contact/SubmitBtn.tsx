788d1578f766465dd8d41663e14d48ff33480888
yujifunfunfun <tyuji051605160516@gmail.com>
2024-01-28 18:17:18 +0900

    送信ボタン修正

--- src/components/pages/Contact/SubmitBtn.tsx
+++ src/components/pages/Contact/SubmitBtn.tsx
@@ -10,12 +10,16 @@ export const SubmitBtn: React.FC<SubmitBtnProps> = ({isSubmitting}) => {
     <Flex w='100%' justifyContent='center'>
       <Button
         borderRadius='30px'
-        bgColor={Color.mouretsuRed}
-        colorScheme='red'
+        bgColor={{ base: Color.mouretsuRed, md: 'white' }}
+        borderColor={Color.mouretsuRed}
+        borderWidth="1px"
+        boxShadow={{ base: 'none', md: '2px 2px 2px rgba(0, 0, 0, 0.2)' }}
+        color={{ base: 'white', md: Color.mouretsuRed }}
         fontSize={{ base: '4.27vw', md: '1.25vw' }}
         w={{ base: '64vw', md: '19.4vw' }}
         type='submit'
         isLoading={isSubmitting}
+        _hover={{ md: { bgColor: Color.mouretsuRed, boxShadow: 'none', color: 'white' } }}
       >
         送信
       </Button>
